6c337ad01b8d922d6bbdf78e4a9b506d57b4698c
Volkan Benli <volkanbenli92@gmail.com>
2021-11-01 17:15:52 -0400

    Adds button with styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,13 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   } else if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return <button className={buttonClass}>{props.children}</button>;
+ }
