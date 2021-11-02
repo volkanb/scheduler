d63275f848a923bb94102e2ab6671bb56c9e2c59
Volkan Benli <volkanbenli92@gmail.com>
2021-11-01 22:38:29 -0400

    Adds conditional styling, props and event handling to InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
