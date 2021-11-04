f8f7f09b06edffa925329bfd20bc342176110813
Volkan Benli <volkanbenli92@gmail.com>
2021-11-04 05:32:27 -0400

    Combines states into an state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,13 +44,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const URL = `http://localhost:8001/api/days`;
     axios.get(URL).then(response => {
-      console.log(response.data);
       setDays(response.data);
     });
   }, []);
@@ -72,8 +77,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
