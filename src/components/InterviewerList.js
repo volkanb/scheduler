5bb491fb65ed990304a733c9b8c1b4a3455634d4
Volkan Benli <volkanbenli92@gmail.com>
2021-11-03 20:48:34 -0400

    Refactor InterviewerList to show interviewers

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,25 @@
 import React from "react";
 import classNames from "classnames";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  
+  const interviewers = props.interviewers.map(interviewer => 
+    <InterviewerListItem 
+      id={interviewer.id}
+      key={interviewer.id} 
+      name={interviewer.name} 
+      avatar={interviewer.avatar}
+      selected={props.interviewer === interviewer.id}
+      setInterviewer={props.setInterviewer}
+    />);
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
     </section>
   );
 }
