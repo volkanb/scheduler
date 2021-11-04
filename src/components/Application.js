9b6b3b724e29dd22c9952be88d4ebe13def1b8a3
Volkan Benli <volkanbenli92@gmail.com>
2021-11-04 05:58:53 -0400

    Displays appointments retrieved from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,47 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "components/Application.scss";
+
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -49,18 +12,22 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-
+  
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
+  
   useEffect(() => {
-    const URL = `http://localhost:8001/api/days`;
-    axios.get(URL).then(response => {
-      setDays(response.data);
-    });
+    Promise.all([
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`),
+    ]).then((all) => {
+      console.log(all);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+    })
   }, []);
+  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const appointmentList = appointments.map(appointment => 
+  const appointmentList = dailyAppointments.map(appointment => 
     <Appointment
       key={appointment.id}    
       {...appointment}
