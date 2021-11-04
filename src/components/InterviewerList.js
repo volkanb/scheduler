c0a913c1f018b188ea24440c8e2f0554a3c3baf1
Volkan Benli <volkanbenli92@gmail.com>
2021-11-03 21:45:22 -0400

    Updates InterviewerList props to use value and onChange

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,16 +1,15 @@
 import React from "react";
-import classNames from "classnames";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => 
     <InterviewerListItem 
-      key={interviewer.id} 
-      name={interviewer.name} 
+      key={interviewer.id}
+      name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={props.interviewer === interviewer.id}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      selected={interviewer.id === props.value}
+      setInterviewer={() => props.onChange(interviewer.id)}    
     />);
 
   return (
