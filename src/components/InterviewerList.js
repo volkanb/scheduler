d9755b8c137862b361396c68aed7826faa24f4f8
Volkan Benli <volkanbenli92@gmail.com>
2021-11-07 23:50:00 -0500

    Validates InterviewerList component props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => 
     <InterviewerListItem 
       key={interviewer.id}
@@ -21,3 +22,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
