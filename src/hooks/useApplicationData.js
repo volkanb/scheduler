869631e443c0d705fc6d93bf8f712c50a3fdea23
Volkan Benli <volkanbenli92@gmail.com>
2021-11-12 04:12:34 -0500

    Removes hardcoded API server URL

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -32,7 +32,7 @@ export default function useApplicationData() {
     };
 
     // Make data persistent after updating remaining spots
-    await axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    await axios.put(`/api/appointments/${id}`, {interview})
       .then(() => {
         const dayId = state.days.filter(day => day.name === state.day)[0].id;
         state.days[dayId - 1].spots = (spotsRemaining() - 1);
@@ -51,7 +51,7 @@ export default function useApplicationData() {
     };
 
     // Make data persistent after updating remaining spots
-    await axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    await axios.delete(`/api/appointments/${id}`)
     .then(() => {
       const dayId = state.days.filter(day => day.name === state.day)[0].id;
       state.days[dayId - 1].spots = (spotsRemaining() + 1);
@@ -63,9 +63,9 @@ export default function useApplicationData() {
   
   useEffect(() => {
     Promise.all([
-      axios.get(`http://localhost:8001/api/days`),
-      axios.get(`http://localhost:8001/api/appointments`),
-      axios.get(`http://localhost:8001/api/interviewers`)
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
